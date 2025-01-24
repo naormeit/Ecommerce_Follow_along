47f14cb0d1dd04edadd7c913c17a7b4088aba4ef
naormeit <necrospre@gmail.com>
2025-01-24 17:34:01 +0530

    changes in milestone 4

--- Backend/Multer.js
+++ Backend/Multer.js
@@ -1,16 +1,18 @@
 const multer = require('multer');
 
 
+// Configure multer storage
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+      cb(null, 'uploads/'); // Define your upload folder
     },
-    filename: function(req, file, cb){
-        const uniqueSuffix = Data.now() + '-' + Math.round.apply(Math.random() * 1e9);
-
-        const filename = file.originalname.split(".").[0];
-        cb(null, filename + "-" + uniqueSuffix + ".png");
+    filename: function(req, file, cb) {
+      const uniqueSuffix =  Date.now() + '-' + Math.round.apply(Math.random() * 1e9);
+       // Define a unique filename
+       const filename = file.originalname.split(".")[0];
+       cb(null,filename + "-" + uniqueSuffix + ".png"); // Define
     },
-})
-
-exports.upload = multer({storage: storage});
+  });
+  
+  // Initialize upload object
+exports.upload = multer({ storage: storage });
