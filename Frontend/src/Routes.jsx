74ffe5fa5af12d2c364ae03e5cee5142b86eb559
naormeit <necrospre@gmail.com>
2025-03-06 11:26:34 +0530

    done

--- Frontend/src/Routes.jsx
+++ Frontend/src/Routes.jsx
@@ -4,10 +4,9 @@ import Home from "./pages/Home.jsx";
 import CreateProduct from "./pages/createProduct.jsx";
 import MyProducts from "./pages/myProducts.jsx";
 import Cart from "./pages/cart.jsx";
-import ProductDetails from "./pages/productDetails.jsx";
-import Profile from "./pages/profile.jsx"
-import CreateAddress from "./pages/createAddress.jsx";
-import SelectAddress from "./pages/SelectAddress.jsx";
-import OrderConfirmation from "./pages/OrderConfirmation.jsx";
-
-export { SignupPage, LoginPage, Home, CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress, SelectAddress, OrderConfirmation };
+import ProductDetails from "./Pages/productDetails.jsx";
+import Profile from "./Pages/profile.jsx"
+import CreateAddress from "./Pages/createAddress.jsx";
+import SelectAddress from "./Pages/selectAddress.jsx";
+import OrderConfirmation from "./Pages/orderconfirmation.jsx";
+export { SignupPage, LoginPage, Home, CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress, SelectAddress, OrderConfirmation  };
